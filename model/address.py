a96e30ef0492c6cb6ccd5acf603d999f85c84f01
Klas Meder Boqvist <github.com@servern.info>
2015-12-29 01:49:10 +0100

    Add basic models for tables

---
+++ model/address.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python2.7
+
+import model
+
+class Address(model.Model):
+	pass
+Address.init_model("addresses", "address_id")
---
+++ model/member.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python2.7
+
+import model
+
+class Member(model.Model):
+	pass
+Member.init_model("members", "member_id")
