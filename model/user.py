e5e0003baca69fb250ab669d11aa22a94b1f11eb
Klas Meder Boqvist <klas.meder.boqvist@evokegaming.com>
2016-01-15 19:53:23 +0100

    Add basic rollback for failed database queries

--- model/user.py
+++ model/user.py
@@ -16,27 +16,39 @@ class User(model.Model):
 			return False
 
 	def delete(self, by):
-		self._db.cursor.execute("UPDATE users SET deleted_by=%s, deleted_at = NOW() where user_id = %d", by, self._id)
-		self._db.conn.commit()
+		try:
+			self._db.cursor.execute("UPDATE users SET deleted_by=%s, deleted_at = NOW() where user_id = %d", by, self._id)
+			self._db.conn.commit()
+		except lib.database.psycopg2.DatabaseError:
+			cls._db.conn.rollback()
+			raise
 
 	def has_access(self, privilige, organization_id=0):
-		self._db.cursor.execute("SELECT 1 FROM user_privileges WHERE user_id = %s AND privilege = %s AND organization_id = %s", (self._id, privilige, organization_id))
-		access = self._db.cursor.rowcount > 0
-		self._db.cursor.execute("SELECT 1 FROM user_privileges WHERE user_id = %s AND (privilege = 'ADMIN' OR privilege = 'READ_EVERYTHING')", (self._id,))
-		access += self._db.cursor.rowcount > 0
-		self._db.conn.commit()
+		try:
+			self._db.cursor.execute("SELECT 1 FROM user_privileges WHERE user_id = %s AND privilege = %s AND organization_id = %s", (self._id, privilige, organization_id))
+			access = self._db.cursor.rowcount > 0
+			self._db.cursor.execute("SELECT 1 FROM user_privileges WHERE user_id = %s AND (privilege = 'ADMIN' OR privilege = 'READ_EVERYTHING')", (self._id,))
+			access += self._db.cursor.rowcount > 0
+			self._db.conn.commit()
+		except lib.database.psycopg2.DatabaseError:
+			cls._db.conn.rollback()
+			raise
 		return access
 
 	def organizations(self):
-		if self.has_access("ADMIN") or self.has_access("READ_EVERYTHING"):
-			self._db.cursor.execute("SELECT organization_id FROM organizations")
-		else:
-			self._db.cursor.execute("SELECT organization_id FROM user_privileges WHERE user_id = %s AND organization_id <> 0 AND privilege = 'ORGANIZATION_ADMIN'", (self["user_id"],))
-		if self._db.cursor.rowcount == 0:
+		try:
+			if self.has_access("ADMIN") or self.has_access("READ_EVERYTHING"):
+				self._db.cursor.execute("SELECT organization_id FROM organizations")
+			else:
+				self._db.cursor.execute("SELECT organization_id FROM user_privileges WHERE user_id = %s AND organization_id <> 0 AND privilege = 'ORGANIZATION_ADMIN'", (self["user_id"],))
+			if self._db.cursor.rowcount == 0:
+				self._db.conn.commit()
+				return tuple()
+			organizations = zip(*self._db.cursor.fetchall())[0]
 			self._db.conn.commit()
-			return tuple()
-		organizations = zip(*self._db.cursor.fetchall())[0]
-		self._db.conn.commit()
+		except lib.database.psycopg2.DatabaseError:
+			cls._db.conn.rollback()
+			raise
 		return organizations
 
 	def set_password(self, passwd):
@@ -44,9 +56,13 @@ class User(model.Model):
 
 	@classmethod
 	def user_from_email(cls, email):
-		cls._db.cursor.execute("SELECT user_id FROM users WHERE email = %s", (email,))
-		user_id = cls._db.cursor.fetchall()[0][0]
-		cls._db.conn.commit()
+		try:
+			cls._db.cursor.execute("SELECT user_id FROM users WHERE email = %s", (email,))
+			user_id = cls._db.cursor.fetchall()[0][0]
+			cls._db.conn.commit()
+		except lib.database.psycopg2.DatabaseError:
+			cls._db.conn.rollback()
+			raise
 		return cls(user_id)
 
 User.init_model("users", "user_id")
